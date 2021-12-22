905483c382f170065638512c96f489ecdc54114a
lucarossi9 <79704292+lucarossi9@users.noreply.github.com>
2021-12-22 11:27:51 +0100

    Add files via upload

--- code/metrics_helper.py
+++ code/metrics_helper.py
@@ -1,28 +1,39 @@
-# HELPER FOR THE METRICS
+
+## HELPER FOR THE METRICS
 
 from sklearn.metrics import mean_squared_error as MSE
 import scipy as sci
 from xgboost import XGBRegressor
 import numpy as np
-from sklearn.metrics import confusion_matrix
+
+"""
+Discussion: Before and after performing undersample and oversample of our dataset, we tried using a different loss to
+train our model on in order to achieve better results in the minority class. As you can see from the final notebooks,
+once the undersamping and oversampling were performed, there was no need to train our model with such metrics since we
+have a dataset almost balanced (70% for the majority class and 30% of the minority class were the proportions which gave
+us the best results). Therefore, this metrics were not used anymore to train the models but they were used only to
+evaluate the goodness of fits of the models after training.
+"""
 
 
-# penalized losses
-
-def penalized_MSE_helper(y_true, y_pred):
+## penalized loss for regression
+def penalized_MSE(y_true, y_pred, decision_boundary=0.7):
     """
-    The function is a helper for the functions penalized_MSE and penalized_MSE_train, the function
-    compute an average loss between the point in the minority and in the majority class
+    The function computes an average loss between the point in the minority and the ones in the majority class
     :param y_true: true labels
     :param y_pred: predicted labels
+    :param decision_boundary: The decision boundary between the two classes, a label bigger than the decision boundary
+    is considered to be belonging to the minority class
     :return: loss = 1/2*MSE(minority class) + 1/2*MSE(majority class)
     """
-    critical_indices = np.where(y_true >= 0.7)[0]
-    print(critical_indices)
-    print(y_true, y_pred)
+    # we find the indices of the points in the minority class
+    critical_indices = np.where(y_true >= decision_boundary)[0]
+
+    # if no such indices were found or if all the points belong to such class we compute a simple MSE
     if critical_indices.shape[0] == 0 | critical_indices.shape[0] == y_true.shape[0]:
         return (1 / 2) * MSE(y_true, y_pred)
     else:
+        # we divide the labels and the predictions based on the value of the
         critical_y = y_true[critical_indices]
         common_y = np.delete(y_true, critical_indices)
         critical_predictions = y_pred[critical_indices]
@@ -30,59 +41,7 @@ def penalized_MSE_helper(y_true, y_pred):
         return (1 / 2) * MSE(critical_y, critical_predictions) + (1 / 2) * MSE(common_y, common_predictions)
 
 
-def penalized_MSE(y_true, y_pred, fitted_lambda=-1.0008549422054305):
-    """
-    The function compute the penalized MSE for the test/val set, in case of the test we did not apply previously boxcox
-    to y_test/y_val while we did for x_test, therefore we scale back the predictions with the inverse boxcox
-    transformation and then we apply the helper function
-    :param y_true: y_test/y_val
-    :param y_pred: y_predicted from x_test or x_val
-    :param fitted_lambda: the parameter lambda which comes from the boxcox transformation, necessary to scale back the
-    y_pred
-    :return: the penalized MSE for the test/val sets
-    """
-    return penalized_MSE_helper(y_true, sci.special.inv_boxcox(y_pred, fitted_lambda))
-
-
-def penalized_MSE_train(y_true, y_pred, fitted_lambda=-1.0008549422054305):
-    """
-    The function compute the penalized MSE for the train set, in case of the train we applied previously boxcox to
-    therefore we scale back the predictions and the true labels with the inverse boxcox transformation and
-    then we apply the helper function
-    :param y_true: y_train
-    :param y_pred: y_predicted from x_train
-    :param fitted_lambda: the parameter lambda which comes from the boxcox transformation, necessary to scale back the
-    y_pred and the y_train
-    :return: the penalized MSE for the train set
-    """
-    return penalized_MSE_helper(sci.special.inv_boxcox(y_true, fitted_lambda),
-                                sci.special.inv_boxcox(y_pred, fitted_lambda))
-
-
-# for error analysis
-
-def score_metrics(x_train, x_test, y_train, y_test, iterations=3):
-    """
-    Returns 2-2d arrays, each of the arrays has 1 row for each iteration.
-    @param x: numpy.ndarray
-    @param y: numpy.ndarray
-    @param model:dictionary
-    @param iterations: int64
-    """
-    training_scores = []
-    testing_scores = []
-    for i in range(iterations):
-        model = XGBRegressor(subsample=0.8999999999999999, n_estimators=500,
-                             max_depth=20, learning_rate=0.01, colsample_bytree=0.7999999999999999,
-                             colsample_bylevel=0.6, obj=penalized_MSE_train)
-        y_train2 = sci.special.inv_boxcox(y_train, fitted_lambda)
-        model.fit(x_train, y_train, eval_metric=penalized_MSE_helper, eval_set=[(x_train, y_train), (x_test, y_test)])
-        training_scores.append(model.evals_result()['validation_0']['rmse'])
-        testing_scores.append(model.evals_result()['validation_1']['rmse'])
-        print(len(testing_scores))
-        print(len(testing_scores[i]))
-    return np.array(training_scores).T, np.array(testing_scores).T
-
+#penalized metric for classification
 
 def weighted_balance_average(y_true, y_pred):
     """
@@ -91,35 +50,44 @@ def weighted_balance_average(y_true, y_pred):
     @param y_pred: the predicted labels
     @return: the accuracy of the prediction
     """
-    # we compute the number of minority class samples in the true labels
+
+    # we compute the number of samples in the minority class using the true labels
     ones_number = np.count_nonzero(y_true == 1.)
+
     # we compute the frequency of the majority and of the minority class in the true labels
     frequency_1 = ones_number / len(y_true)
     frequency_0 = 1 - frequency_1
+
     # we compute the sum of the inverse frequencies
     sum_inverse_frequencies = 1 / frequency_0 + 1 / frequency_1
+
     # we define the weights of our accuracy metric as the inverse of the frequency of a class times times a corrective
     # term so that the weights are summing up to 1
     weight_0 = 1 / (frequency_0 * sum_inverse_frequencies)
     weight_1 = 1 / (frequency_1 * sum_inverse_frequencies)
+
     # we calculate the standard confusion matrix, our metric is a balanced average of the trace elements of C
     C = confusion_matrix(y_true, y_pred)
+
     # the accuracy is weight_0 * accuracy_0 + weight_1 * accuracy_1
     return weight_0 * C[0, 0] / (C[0, 0] + C[0, 1]) + weight_1 * C[1, 1] / (C[1, 1] + C[1, 0])
 
 
+# Useful function for evaluating the classification model
 def return_accuracy(y_test, y_pred, verbose=1):
     """
     This function computes the standard accuracy of a prediction
     @param y_test: the true labels
     @param y_pred: the predicted labels
-    @param verbose: verbose parameter
+    @param verbose: verbose parameter, if verbose=1 we print more details
     @return: the accuracy of the prediction
     """
     # we compute the confusion matrix
     C = confusion_matrix(y_test, y_pred)
+
     # we compute the accuracy
     accuracy = np.trace(C) / len(y_test)
+
     if verbose == 1:
         # print all the elements of the matrix and the accuracies on the two classes
         print("The number of true negatives is:", C[0, 0])
@@ -130,3 +98,27 @@ def return_accuracy(y_test, y_pred, verbose=1):
         print("The accuracy on the 1's is ", 1 / np.sum(y_test == 1) * (np.sum(y_test == 1) - C[1, 0]))
         print("The accuracy on the 0's is ", 1 / np.sum(y_test == 0) * (np.sum(y_test == 0) - C[0, 1]))
     return accuracy
+
+
+# Useful function for the plot of the curves of the losses to evaluate if we are overfitting or not
+def score_metrics_regression(x_train, x_test, y_train, y_test, model):
+  """
+  Returns 2-2d arrays, each of the arrays has 1 row for each iteration, the first array contains the training losses for
+  each iteration, the second array contains the testing losses for each iteration. This function will be useful to
+  understand if the model chosen is overfitting or underfitting.
+  @param x_train: numpy.ndarray containing the features of the training set
+  @param x_test: numpy.ndarray containing the features of the test set
+  @param y_train: numpy.ndarray containing the labels of the training set
+  @param y_test: numpy.ndarray containing the labels of the test set
+  @param model: the regression model we are evaluating
+  """
+  training_scores=[]
+  testing_scores=[]
+  # we fit the model
+  model.fit(x_train, y_train, eval_metric = "rmse", eval_set= [(x_train,y_train), (x_test, y_test)])
+
+  # we take the results for the training and test set and we append to the lists
+  training_scores.append(model.evals_result()['validation_0']['rmse'])
+  testing_scores.append(model.evals_result()['validation_1']['rmse'])
+
+  return np.array(training_scores).T, np.array(testing_scores).T
